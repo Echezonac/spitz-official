e8d114935bbcdcaccadb77f175b33a86a5fa7db2
Echezonac <hampoherobede@gmail.com>
2022-07-10 16:29:16 +0100

    css and bootstrap setup

--- home_app/views.py
+++ home_app/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render
 from .models import *
 
 
 def home(request):
-    spitz = Spit.objects.all()
-    return render(request,'pages/index.html',{'spitz':spitz})
+    context = {}
+    return render(request,'pages/index.html',context)
 
-def spit_detail_view(request):
-    spitz = get_object_or_404(Spit,id=id)
-    photos = SpitImage.objects.filter(spitz=spitz)
-    return render(request,'pages/spitz_detail.html',{'spitz':spitz,'photos':photos})

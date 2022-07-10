65c9146bd078492af0c6bb9c71ac26a6ef7c0831
Echezonac <hampoherobede@gmail.com>
2022-07-10 16:15:12 +0100

    corrected the .gitignore file

--- home_app/models.py
+++ home_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# # Create your models here.
+class Spit(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000,blank=True)
+    time = models.TimeField(auto_now=True)
+    images = models.ImageField(blank=True)
+    
+    def __str__(self):
+        return self.author + ' ' + self.time
+    
+class SpitImage(models.Model):
+    spitz = models.ForeignKey(Spit, default=None,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='img/')
+    
+    def __str__(self):
+        return self.spitz.author + ' ' + self.spitz.time
+
+    
+
+    
--- home_app/views.py
+++ home_app/views.py
@@ -1,7 +1,12 @@
-import imp
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from .models import *
 
-# Create your views here.
+
 def home(request):
-    return HttpResponse("Hello Spitz team, everything setup")
+    spitz = Spit.objects.all()
+    return render(request,'pages/index.html',{'spitz':spitz})
+
+def spit_detail_view(request):
+    spitz = get_object_or_404(Spit,id=id)
+    photos = SpitImage.objects.filter(spitz=spitz)
+    return render(request,'pages/spitz_detail.html',{'spitz':spitz,'photos':photos})
